d11c42a8821db180fe126cbf893155ab3c140071
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-12-06 11:19:45 +0600

    Blog App Featured Post Design [Completed]

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -1,6 +1,34 @@
+// configuration
+import Image from "next/image";
+
 // css file
 import styles from "./featured.module.css";
 
 export const Featured = () => {
-    return <div className={styles.container}>Featured</div>;
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                <b>Hey, Dev scroll is here!</b> Discover infinite stories and
+                creative ideas.
+            </h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src={"/p1.jpeg"} alt="" width={688} height={500} />
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.postTitle}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit.
+                    </h1>
+                    <p className={styles.postDesc}>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Recusandae quis repellendus suscipit illo saepe
+                        nemo delectus, id totam omnis, fugit beatae, ipsa
+                        impedit? Soluta maiores esse modi similique ut quaerat!
+                    </p>
+                    <button className={styles.button}>Read More</button>
+                </div>
+            </div>
+        </div>
+    );
 };
