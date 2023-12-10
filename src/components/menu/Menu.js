200b3d4ac374a56c25b2f0fb81cb9c55e3e11297
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-12-10 10:58:53 +0600

    Side Menu Component Design [Completed]

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,6 +1,22 @@
 // css file
+import MenuCategories from "../menuCategories/MenuCategories";
+import MenuPosts from "../menuPosts/MenuPosts";
 import styles from "./menu.module.css";
 
 export const Menu = () => {
-    return <div className={styles.container}>Menu</div>;
+    return (
+        <div className={styles.container}>
+            <MenuPosts withImage={false} />
+
+            {/* Middle section */}
+            <section>
+                <h2 className={styles.subTitle}>Discover by topic</h2>
+                <h1 className={styles.title}>Categories</h1>
+                <MenuCategories />
+            </section>
+
+            {/* Final section */}
+            <MenuPosts withImage={true} />
+        </div>
+    );
 };
